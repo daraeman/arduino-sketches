ad5b7f3c2facf5e78144139fe2ae6949e08cea15
daraeman <daraeman@gmail.com>
2015-09-30 19:10:12 -0700

    updated with single, double phase, and half step

--- bipolar_stepper/bipolar_stepper.ino
+++ bipolar_stepper/bipolar_stepper.ino
@@ -1,21 +1,61 @@
 /*
 	Bipolar Stepper motor control
-	Full step sequence only
 */
 int in1 = 2; // coil 1a
 int in2 = 3; // coil 1b
 int in3 = 4; // coil 2a
 int in4 = 5; // coil 2b
-int wait = 500; // time to wait between steps
-int pulse_delay = 10; // time to wait before de-energizing coils
+int wait = 300; // time to wait between steps
+// time to wait before de-energizing the
+// coils, letting the rotor move positions.
+// some motors need more pulse delay than others
+int pulse_delay = 10;
 int current_sequence = 0;
-int sequence_count = 4; // the number of steps in the "sequence" array
+int sequence_count = 8; // the number of steps in the "sequence" array
+
+/*
+	wave drive (single phase)
+	for some reason on the cheap stepper motor I tested,
+	each step is actually two steps.
+	Also coil one is forwards,
+	coil two is backwards
+*/
+/*
 int sequence[4][4] = {
-	{1, 0, 1, 0},
-	{0, 1, 0, 1},
-	{0, 1, 1, 0},
-	{1, 0, 0, 1}
+	{1, 0, 0, 0},
+	{0, 1, 0, 0},
+//	{0, 0, 1, 0},
+//	{0, 0, 0, 1}
 };
+*/
+
+/*
+	two phase
+	full steps
+*/
+/*
+int sequence[4][4] = {
+	{1, 0, 0, 1},
+	{1, 0, 1, 0},
+	{0, 1, 1, 0},
+	{0, 1, 0, 1}
+};
+
+/*
+	half step
+*/
+
+int sequence[8][4] = {
+	{1, 0, 0, 1},
+	{0, 0, 0, 1},
+	{0, 1, 0, 1},
+	{0, 1, 0, 0},
+	{0, 1, 1, 0},
+	{0, 0, 1, 0},
+	{1, 0, 1, 0},
+	{1, 0, 0, 0}
+};
+
 
 bool stepper( bool coil_1_a, bool coil_1_b, bool coil_2_a, bool coil_2_b, bool lock ) {
 	digitalWrite( in1, coil_1_a ? HIGH : LOW );
@@ -24,8 +64,9 @@ bool stepper( bool coil_1_a, bool coil_1_b, bool coil_2_a, bool coil_2_b, bool l
 	digitalWrite( in4, coil_2_b ? HIGH : LOW );
 
 	// if lock is true, this will keep the coils energized
-	// keeping the motor in place, steppers can get quite
-	// hot if left like this
+	// keeping the motor in place, best to turn this off if
+	// it is not necessary since steppers get really hot if
+	// left on
 	if ( ! lock ) {
 		delay( pulse_delay );
 		digitalWrite( in1, LOW );
@@ -42,7 +83,7 @@ bool forward( bool lock ) {
 }
 
 bool reverse( bool lock ) {
-	current_sequence = ( (current_sequence - 1) < 0 ) ? (sequence_count - 1) : (current_sequence - 1);
+	current_sequence = ( current_sequence == 0 ) ? (sequence_count - 1) : (current_sequence - 1);
 	int *seq = sequence[ current_sequence ];
 	stepper( seq[0], seq[1], seq[2], seq[3], lock );
 }
@@ -56,28 +97,14 @@ void setup() {
 
 void loop() {
 
-	forward( 0 );
-	delay( wait );
+	for ( int i = 0; i < 24; i++ ) {
+		forward( 0 );
+		delay( wait );
+	}
 
-	forward( 0 );
-	delay( wait );
+	for ( int i = 0; i < 24; i++ ) {
+		reverse( 0 );
+		delay( wait );
+	}
 
-	forward( 0 );
-	delay( wait );
-
-	forward( 0 );
-	delay( wait );
-
-
-	reverse( 0 );
-	delay( wait );
-
-	reverse( 0 );
-	delay( wait );
-
-	reverse( 0 );
-	delay( wait );
-
-	reverse( 0 );
-	delay( wait );
 }
